28739f44791f213373dd104af58e12f21fe8041b
ritesh kasat <ritesh@riteshs-MacBook-Pro.local>
2019-01-26 17:46:56 -0800

    Change app name to HelloWorld

---
+++ src/HelloWorldApp.js
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div>
+        Hello
+      </div>  
+    );
+  }
+}
+
+export default HelloWorld;
